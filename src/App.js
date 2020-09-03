d60e9b6addf337f1ceb580f6e1c842f42118770d
Yfer <yago@codeby.com.br>
2020-09-03 12:36:25 -0300

    Dex test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AuthRoute from "./AuthRoute";
-import "./App.css";
 import Login from "./pages/Login";
 import List from "./pages/List";
 
